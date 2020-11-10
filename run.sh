c9d9f367d46f3499da4d022c8c2a242f97e4a7b9
Anton Patsev <patsev.anton@gmail.com>
2020-11-10 11:27:01 +0600

    init

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+terraform init
+time terraform apply -auto-approve
+# sleep 60
+
+# ansible-playbook disable_selinux.yml
+# ansible-galaxy install andrewrothstein.etcd-cluster
+# ansible-playbook etcd_cluster.yml
+
+# Zookeeper
+# ansible-galaxy install andrewrothstein.zookeeper-cluster
+# # https://github.com/andrewrothstein/ansible-zookeeper-cluster/issues/9
+# ansible-galaxy install andrewrothstein.zookeeper --force
+# ansible-playbook install_zookeeper_cluster.yml
